16e57f909ce1cc692c7023e6af259eafab8ea4ef
First Last <rubenesau.gg@gmail.com>
2020-12-31 09:33:44 -0600

    correction of the reverse method

--- Exercises/reverse_integer.py
+++ Exercises/reverse_integer.py
@@ -1,11 +1,26 @@
 class Solution:
     def reverse(self, x: int) -> int:
+        if x == 0 or x >= 2**31-1 or x <= -2**31:  # if x is 0 or outside our bounds, then return 0
+            return 0
+
+        negative = False
+        if x < 0:
+            negative = True
+            x *= -1
+
         num = 0
         while x > 0:
             residuo = x % 10
             #print(f"residuo: {residuo}")
             x = x//10
             num = num*10+residuo
+
+        if negative:
+            num *= -1
+
+        if num >= 2**31-1 or num <= -2**31:  # check if reversed x is now outside bounds
+            return 0
+
         return num
 
     def reverse_going_backwards(self, x: int) -> int:
@@ -13,6 +28,6 @@ class Solution:
 
 
 x = Solution()
-print(x.reverse(123456789))
+print(x.reverse(-123456789))
 
 print(x.reverse_going_backwards(123456789))
